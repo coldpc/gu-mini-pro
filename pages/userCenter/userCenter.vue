082657f7b5ea60efe88f03d18325f20253b26b1c
DESKTOP-FUP91L1\pccold <928343883@qq.com>
2018-12-13 00:13:40 +0800

    个人中心

--- pages/userCenter/userCenter.vue
+++ pages/userCenter/userCenter.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		
+		<div class="top-con">
+			<image mode="aspectFill" class=".icon{
+				borderr-radius: 50%;
+				width: 50px;
+				height: 50px
+			}"></image>
+		</div>
 	</view>
 </template>
 
@@ -15,5 +21,14 @@
 </script>
 
 <style>
-
+	.top-con{
+		text-align: center;
+		padding: 30px;
+	}
+	.icon{
+		border-radius: 50%;
+		width: 50px;
+		height: 50px;
+		border: 1px solid #ccc;
+	}
 </style>
